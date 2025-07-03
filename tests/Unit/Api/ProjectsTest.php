3cc5d384d3f15579d408b811ede47b7aa93a0de1
Ali Youssef Kayed <3likayed@gmail.com>
2025-07-03 15:44:45 +0300

    running formatter

--- tests/Unit/Api/ProjectsTest.php
+++ tests/Unit/Api/ProjectsTest.php
@@ -6,78 +6,73 @@ use KayedSpace\N8n\Enums\RequestMethod;
 use KayedSpace\N8n\Facades\N8nClient;
 
 it('creates a project', function () {
-    Http::fake(fn () => Http::response(['id' => 'p1'], 201));
+    Http::fake(fn() => Http::response(['id' => 'p1'], 201));
 
     $payload = ['name' => 'Project 1'];
-    $resp    = N8nClient::projects()->create($payload);
+    $resp = N8nClient::projects()->create($payload);
 
     expect($resp)->toMatchArray(['id' => 'p1']);
 
     $url = Config::get('n8n.api.base_url');
 
-    Http::assertSent(fn ($r) =>
-        $r->method() === RequestMethod::Post->value
+    Http::assertSent(fn($r) => $r->method() === RequestMethod::Post->value
         && $r->url() === "{$url}/projects"
         && $r->data() === $payload
     );
 });
 
 it('lists projects without cursor', function () {
-    Http::fake(fn () => Http::response(['items' => []], 200));
+    Http::fake(fn() => Http::response(['items' => []], 200));
 
     N8nClient::projects()->list(50);
 
     $url = Config::get('n8n.api.base_url');
 
-    Http::assertSent(fn ($r) =>
-        $r->method() === RequestMethod::Get->value
+    Http::assertSent(fn($r) => $r->method() === RequestMethod::Get->value
         && $r->url() === "{$url}/projects?limit=50"
     );
 });
 
 it('lists projects with cursor', function () {
-    Http::fake(fn () => Http::response(['items' => []], 200));
+    Http::fake(fn() => Http::response(['items' => []], 200));
 
     N8nClient::projects()->list(25, 'abc');
 
     $url = Config::get('n8n.api.base_url');
 
-    Http::assertSent(fn ($r) =>
-        $r->method() === RequestMethod::Get->value
+    Http::assertSent(fn($r) => $r->method() === RequestMethod::Get->value
         && $r->url() === "{$url}/projects?limit=25&cursor=abc"
     );
 });
 
 it('updates a project', function () {
-    Http::fake(fn () => Http::response([], 204));
+    Http::fake(fn() => Http::response([], 204));
 
     $payload = ['name' => 'Updated'];
     N8nClient::projects()->update('p1', $payload);
 
     $url = Config::get('n8n.api.base_url');
 
-    Http::assertSent(fn ($r) =>
-        $r->method() === RequestMethod::Put->value
+    Http::assertSent(fn($r) => $r->method() === RequestMethod::Put->value
         && $r->url() === "{$url}/projects/p1"
         && $r->data() === $payload
     );
 });
 
 it('deletes a project', function () {
-    Http::fake(fn () => Http::response([], 204));
+    Http::fake(fn() => Http::response([], 204));
 
     N8nClient::projects()->delete('p1');
 
     $url = Config::get('n8n.api.base_url');
 
-    Http::assertSent(fn ($r) =>
-        $r->method() === RequestMethod::Delete->value
+    Http::assertSent(fn($r) => $r->method() === RequestMethod::Delete->value
         && $r->url() === "{$url}/projects/p1"
     );
 });
 
 it('adds users to a project', function () {
-    Http::fake(fn () => Http::response([], 204));
+    Http::fake(fn() => Http::response([], 204));
 
     $relations = [
         ['userId' => 'u1', 'role' => 'editor'],
@@ -88,22 +83,20 @@ it('adds users to a project', function () {
 
     $url = Config::get('n8n.api.base_url');
 
-    Http::assertSent(fn ($r) =>
-        $r->method() === RequestMethod::Post->value
+    Http::assertSent(fn($r) => $r->method() === RequestMethod::Post->value
         && $r->url() === "{$url}/projects/p1/users"
         && $r['relations'] === $relations
     );
 });
 
 it('changes a user role in project', function () {
-    Http::fake(fn () => Http::response([], 204));
+    Http::fake(fn() => Http::response([], 204));
 
     N8nClient::projects()->changeUserRole('p1', 'u1', 'admin');
 
     $url = Config::get('n8n.api.base_url');
 
-    Http::assertSent(fn ($r) =>
-        $r->method() === RequestMethod::Patch->value
+    Http::assertSent(fn($r) => $r->method() === RequestMethod::Patch->value
         && $r->url() === "{$url}/projects/p1/users/u1"
         && $r['role'] === 'admin'
     );
@@ -111,14 +104,13 @@ it('changes a user role in project', function () {
 
 
 it('removes a user from project', function () {
-    Http::fake(fn () => Http::response([], 204));
+    Http::fake(fn() => Http::response([], 204));
 
     N8nClient::projects()->removeUser('p1', 'u1');
 
     $url = Config::get('n8n.api.base_url');
 
-    Http::assertSent(fn ($r) =>
-        $r->method() === RequestMethod::Delete->value
+    Http::assertSent(fn($r) => $r->method() === RequestMethod::Delete->value
         && $r->url() === "{$url}/projects/p1/users/u1"
     );
 });
